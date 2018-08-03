382371aa1b42b1a3584ea77dad1b8d9266094c73
Sebastian Pawlak <spawlak@atlassian.com>
2018-08-03 15:39:16 +0200

    JPT-670: Configure Gradle

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## JPT AWS infrastructure
 
 This module defines provisioning of JPT infrastructure in AWS.
+
+### Version and release management
+This module uses [gradle-release](https://bitbucket.org/atlassian/gradle-release/src/master/) plugin help with 
+version and release management. 
+Please refer to [plugin docs](https://bitbucket.org/atlassian/gradle-release/src/release-0.0.2/README.md) for more information.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,38 @@
-plugins {
-    kotlin("jvm").version(Versions.kotlin)
-    maven
-}
+val kotlinVersion = "1.2.30"
 
-maven {
-    group = "com.atlassian.test.performance"
-    version = "0.0.1-SNAPSHOT"
+plugins {
+    kotlin("jvm").version("1.2.30")
+    id("com.atlassian.performance.tools.gradle-release").version("0.0.2")
 }
 
 dependencies {
-    compile(Libs.infrastructure)
-    compile(Libs.awsResources)
-    compile(Libs.tasks)
-    compile(Libs.kotlinStandard)
-    compile(Libs.json)
+    tools("infrastructure", "0.0.2")
+    tools("aws-resources", "0.0.1")
+    tools("jvm-tasks", "0.0.2")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8:$kotlinVersion")
+    compile("org.glassfish:javax.json:1.1")
     compile("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.4")
-    testCompile(Libs.junit)
-    testCompile(Libs.hamcrest)
+    testCompile("junit:junit:4.12")
+    testCompile("org.hamcrest:hamcrest-library:1.3")
+    log4j(
+        "api",
+        "core",
+        "slf4j-impl"
+    ).forEach { compile(it) }}
 
-    Libs.log4jCore().forEach { compile(it) }
+fun DependencyHandler.tools(
+    module: String,
+    version: String
+) {
+    compile("com.atlassian.performance.tools:$module:$version")
 }
+
+fun log4j(
+    vararg modules: String
+): List<String> = modules.map { module ->
+    "org.apache.logging.log4j:log4j-$module:2.10.0"
+}
+
+val wrapper = tasks["wrapper"] as Wrapper
+wrapper.gradleVersion = "4.9"
+wrapper.distributionType = Wrapper.DistributionType.ALL
