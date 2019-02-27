cddefccd08d7ed77a8f5fe36cb2f1e72c9a1d86c
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-02-27 07:57:00 +0100

    Setup Travis CI
    Travis stops the build when a job takes longer than 50 minutes.
    See https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts.
    
    I'm using a small dataset in AwsDatasetIT to reduce build time.
    The dataset contains Jira with more active users than the license
    allows, so it'll not work for other tests.

--- src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/AwsDatasetIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/AwsDatasetIT.kt
@@ -1,16 +1,21 @@
 package com.atlassian.performance.tools.awsinfrastructure
 
+import com.amazonaws.regions.Regions
+import com.atlassian.performance.tools.aws.api.StorageLocation
 import com.atlassian.performance.tools.awsinfrastructure.IntegrationTestRuntime.aws
 import com.atlassian.performance.tools.awsinfrastructure.IntegrationTestRuntime.taskWorkspace
 import com.atlassian.performance.tools.awsinfrastructure.api.DatasetCatalogue
+import com.atlassian.performance.tools.infrastructure.api.dataset.Dataset
 import com.atlassian.performance.tools.ssh.api.Ssh
 import org.junit.Test
+import java.net.URI
+import java.time.Duration.ofMinutes
 import java.util.*
 
 class AwsDatasetIT {
 
     private val workspace = taskWorkspace.isolateTest(javaClass.simpleName)
-    private val sourceDataset = DatasetCatalogue().largeJira()
+    private val sourceDataset = smallJiraSeven()
 
     @Test
     fun shouldRemoveBackups() {
@@ -32,4 +37,15 @@ class AwsDatasetIT {
                     }
             }
     }
+
+    private fun smallJiraSeven(): Dataset = DatasetCatalogue().custom(
+        location = StorageLocation(
+            URI("s3://jpt-custom-datasets-storage-a008820-datasetbucket-1sjxdtrv5hdhj/")
+                .resolve("af4c7d3b-925c-464c-ab13-79f615158316"),
+            Regions.EU_WEST_1
+        ),
+        label = "7k issues",
+        databaseDownload = ofMinutes(5),
+        jiraHomeDownload = ofMinutes(5)
+    )
 }
