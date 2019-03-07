0d1d7fb3a73b34137936b20b6939fc03112f3c85
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-03-07 14:47:37 +0100

    JPERF-412 JPERF-413 Download and unzip datasets in parallel

--- src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/DatasetCatalogue.kt
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/DatasetCatalogue.kt
@@ -1,11 +1,11 @@
 package com.atlassian.performance.tools.awsinfrastructure.api
 
-import com.amazonaws.regions.Regions
 import com.atlassian.performance.tools.aws.api.StorageLocation
 import com.atlassian.performance.tools.awsinfrastructure.S3DatasetPackage
 import com.atlassian.performance.tools.infrastructure.api.database.MySqlDatabase
 import com.atlassian.performance.tools.infrastructure.api.dataset.Dataset
 import com.atlassian.performance.tools.infrastructure.api.dataset.FileArchiver
+import com.atlassian.performance.tools.infrastructure.api.dataset.HttpDatasetPackage
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomePackage
 import java.net.URI
 import java.time.Duration
@@ -16,27 +16,43 @@ class DatasetCatalogue {
     @Deprecated(message = "Use largeJiraSeven() instead")
     fun largeJira() = largeJiraSeven()
 
-    fun largeJiraSeven(): Dataset = custom(
-        location = StorageLocation(
-            URI("s3://jpt-custom-datasets-storage-a008820-datasetbucket-1sjxdtrv5hdhj/")
-                .resolve("dataset-d4684761-116b-49ae-9cce-e45cecdcae2a"),
-            Regions.EU_WEST_1
-        ),
-        label = "2M issues, format 7",
-        databaseDownload = ofMinutes(17),
-        jiraHomeDownload = ofMinutes(21)
-    )
+    fun largeJiraSeven(): Dataset {
+        val bucketUri = URI("https://s3-eu-west-1.amazonaws.com/jpt-custom-datasets-storage-a008820-datasetbucket-1sjxdtrv5hdhj/")
+        return Dataset(
+            label = "2M issues, format 7",
+            database = MySqlDatabase(
+                HttpDatasetPackage(
+                    uri = bucketUri.resolve("dataset-d4684761-116b-49ae-9cce-e45cecdcae2a/database.tar.bz2"),
+                    downloadTimeout = ofMinutes(17)
+                )
+            ),
+            jiraHomeSource = JiraHomePackage(
+                HttpDatasetPackage(
+                    uri = bucketUri.resolve("dataset-d4684761-116b-49ae-9cce-e45cecdcae2a/jirahome.tar.bz2"),
+                    downloadTimeout = ofMinutes(21)
+                )
+            )
+        )
+    }
 
-    fun largeJiraEight(): Dataset = custom(
-        location = StorageLocation(
-            URI("s3://jpt-custom-datasets-storage-a008820-datasetbucket-dah44h6l1l8p/")
-                .resolve("dataset-631c70d4-084b-455c-9785-b01068b9f07c"),
-            Regions.EU_CENTRAL_1
-        ),
-        label = "2M issues, format 8",
-        databaseDownload = ofMinutes(17),
-        jiraHomeDownload = ofMinutes(21)
-    )
+    fun largeJiraEight(): Dataset {
+        val bucketUri = URI("https://s3.eu-central-1.amazonaws.com/jpt-custom-datasets-storage-a008820-datasetbucket-dah44h6l1l8p/")
+        return Dataset(
+            label = "2M issues, format 8",
+            database = MySqlDatabase(
+                HttpDatasetPackage(
+                    uri = bucketUri.resolve("dataset-631c70d4-084b-455c-9785-b01068b9f07c/database.tar.bz2"),
+                    downloadTimeout = ofMinutes(17)
+                )
+            ),
+            jiraHomeSource = JiraHomePackage(
+                HttpDatasetPackage(
+                    uri = bucketUri.resolve("dataset-631c70d4-084b-455c-9785-b01068b9f07c/jirahome.tar.bz2"),
+                    downloadTimeout = ofMinutes(21)
+                )
+            )
+        )
+    }
 
     fun custom(
         location: StorageLocation,
