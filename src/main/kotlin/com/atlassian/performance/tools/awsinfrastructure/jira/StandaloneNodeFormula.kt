c4093ec48ca61cf1530849a8f5b2ca1daa9576aa
Michal Wyrzykowski <mwyrzykowski@atlassian.com>
2019-04-16 11:22:32 +0200

    JPERF-457 Install Jira and download dataset at the same time

--- src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/StandaloneNodeFormula.kt
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/StandaloneNodeFormula.kt
@@ -3,6 +3,7 @@ package com.atlassian.performance.tools.awsinfrastructure.jira
 import com.atlassian.performance.tools.aws.api.Storage
 import com.atlassian.performance.tools.awsinfrastructure.AwsCli
 import com.atlassian.performance.tools.awsinfrastructure.api.hardware.Computer
+import com.atlassian.performance.tools.concurrency.api.submitWithLogContext
 import com.atlassian.performance.tools.infrastructure.api.Sed
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraGcLog
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
@@ -12,12 +13,13 @@ import com.atlassian.performance.tools.infrastructure.api.os.Ubuntu
 import com.atlassian.performance.tools.infrastructure.api.distribution.ProductDistribution
 import com.atlassian.performance.tools.jvmtasks.api.Backoff
 import com.atlassian.performance.tools.jvmtasks.api.IdempotentAction
-import com.atlassian.performance.tools.jvmtasks.api.TaskTimer
 import com.atlassian.performance.tools.ssh.api.Ssh
 import com.atlassian.performance.tools.ssh.api.SshConnection
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.time.Duration
+import java.util.concurrent.Executors
 
 internal class StandaloneNodeFormula(
     private val jiraHomeSource: JiraHomeSource,
@@ -37,53 +39,74 @@ internal class StandaloneNodeFormula(
 
     override fun provision(): StandaloneStoppedNode {
         logger.info("Setting up $name...")
-
         ssh.newConnection().use { connection ->
             computer.setUp(connection)
-            val unpackedProduct = productDistribution.install(connection, ".")
-            val jiraHome = TaskTimer.time("download Jira home") {
-                jiraHomeSource.download(connection)
+        }
+        val executor = Executors.newCachedThreadPool(
+            ThreadFactoryBuilder()
+                .setNameFormat("install-product-with-dataset-${config.name}-%d")
+                .build()
+        )
+
+        try {
+            val unpackedProductFuture = executor.submitWithLogContext("install product") {
+                ssh.newConnection().use { connection ->
+                    productDistribution.install(connection, ".")
+                }
             }
-            replaceDbconfigUrl(connection, "$jiraHome/dbconfig.xml")
-            SetenvSh(unpackedProduct).setup(
-                connection = connection,
-                config = config,
-                gcLog = JiraGcLog(unpackedProduct),
-                jiraIp = ssh.host.ipAddress
-            )
-            connection.execute("echo jira.home=`realpath $jiraHome` > $unpackedProduct/atlassian-jira/WEB-INF/classes/jira-application.properties")
-            connection.execute("echo jira.autoexport=false > $jiraHome/jira-config.properties")
-            downloadMysqlConnector(
-                "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz",
-                connection
-            )
-            connection.execute("tar -xzf mysql-connector-java-5.1.40.tar.gz")
-            connection.execute("cp mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar $unpackedProduct/lib")
-            AwsCli().download(pluginsTransport.location, connection, target = "$jiraHome/plugins/installed-plugins")
 
-            jdk.install(connection)
-            val osMetrics = ubuntu.metrics(connection)
+            val jiraHomeFuture = executor.submitWithLogContext("download Jira home") {
+                ssh.newConnection().use { connection ->
+                    jiraHomeSource.download(connection)
+                }
+            }
 
-            config.splunkForwarder.jsonifyLog4j(
-                connection,
-                log4jPropertiesPath = "$unpackedProduct/atlassian-jira/WEB-INF/classes/log4j.properties"
-            )
-            config.splunkForwarder.run(connection, name, "/home/ubuntu/jirahome/log")
-            config.profiler.install(connection)
-            logger.info("$name is set up")
+            val jiraHome = jiraHomeFuture.get()
+            val unpackedProduct = unpackedProductFuture.get()
+            ssh.newConnection().use { connection ->
+                replaceDbconfigUrl(connection, "$jiraHome/dbconfig.xml")
+                SetenvSh(unpackedProduct).setup(
+                    connection = connection,
+                    config = config,
+                    gcLog = JiraGcLog(unpackedProduct),
+                    jiraIp = ssh.host.ipAddress
+                )
+                connection.execute("echo jira.home=`realpath $jiraHome` > $unpackedProduct/atlassian-jira/WEB-INF/classes/jira-application.properties")
+                connection.execute("echo jira.autoexport=false > $jiraHome/jira-config.properties")
+                downloadMysqlConnector(
+                    "https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz",
+                    connection
+                )
+                connection.execute("tar -xzf mysql-connector-java-5.1.40.tar.gz")
+                connection.execute("cp mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar $unpackedProduct/lib")
+                AwsCli().download(pluginsTransport.location, connection, target = "$jiraHome/plugins/installed-plugins")
 
-            return StandaloneStoppedNode(
-                name = name,
-                jiraHome = jiraHome,
-                analyticLogs = jiraHome,
-                resultsTransport = resultsTransport,
-                unpackedProduct = unpackedProduct,
-                osMetrics = osMetrics,
-                ssh = ssh,
-                launchTimeouts = config.launchTimeouts,
-                jdk = jdk,
-                profiler = config.profiler
-            )
+                jdk.install(connection)
+                val osMetrics = ubuntu.metrics(connection)
+
+                config.splunkForwarder.jsonifyLog4j(
+                    connection,
+                    log4jPropertiesPath = "$unpackedProduct/atlassian-jira/WEB-INF/classes/log4j.properties"
+                )
+                config.splunkForwarder.run(connection, name, "/home/ubuntu/jirahome/log")
+                config.profiler.install(connection)
+                logger.info("$name is set up")
+
+                return StandaloneStoppedNode(
+                    name = name,
+                    jiraHome = jiraHome,
+                    analyticLogs = jiraHome,
+                    resultsTransport = resultsTransport,
+                    unpackedProduct = unpackedProduct,
+                    osMetrics = osMetrics,
+                    ssh = ssh,
+                    launchTimeouts = config.launchTimeouts,
+                    jdk = jdk,
+                    profiler = config.profiler
+                )
+            }
+        } finally {
+            executor.shutdownNow()
         }
     }
 
