2387954ec5e113ac89550cf37d59d00892f4df75
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2023-02-09 16:39:39 +0100

    JPERF-997: Return AWS networks as resources
    ProvisionedNetwork *inherits* from Network to be transparent for
    existing `NetworkFormula.provision` callers.
    I don't like inheritance either. Next commit I'll try the other way.

---
+++ src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/network/NetworkFormulaIT.kt
@@ -0,0 +1,22 @@
+package com.atlassian.performance.tools.awsinfrastructure.api.network
+
+import com.atlassian.performance.tools.aws.api.Investment
+import com.atlassian.performance.tools.awsinfrastructure.IntegrationTestRuntime.aws
+import org.junit.Test
+import java.time.Duration
+import java.util.concurrent.TimeUnit.MINUTES
+
+class NetworkFormulaIT {
+
+    @Test
+    fun shouldReleaseResources() {
+        aws.cleanLeftovers()
+        val investment = Investment(
+            useCase = "test NetworkFormula",
+            lifespan = Duration.ofMinutes(10)
+        )
+
+        val network = NetworkFormula(investment, aws).provision()
+        network.release().get(4, MINUTES)
+    }
+}
