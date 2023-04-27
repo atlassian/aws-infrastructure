9c1a56e7d8341a4850ea3f86074b296159572d3d
Matt Cochrane <mcochrane@atlassian.com>
2023-04-27 10:58:12 +1000

    Make JiraSharedStorageResource api so jpt can us it.

--- src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/jira/JiraSharedStorageResource.kt
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/jira/JiraSharedStorageResource.kt
@@ -1,8 +1,8 @@
-package com.atlassian.performance.tools.awsinfrastructure.jira.home;
+package com.atlassian.performance.tools.awsinfrastructure.api.jira;
 
 /**
  * Describes a set of resources used by Jira and stored in shared storage (eg. shared home or S3).
  */
-public enum JiraSharedStorageResource {
+enum class JiraSharedStorageResource {
     AVATARS, ATTACHMENTS
 }
