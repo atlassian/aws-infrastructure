533e6fd681765c6aabb76144ba6e02a007039579
Matt Cochrane <mcochrane@atlassian.com>
2023-05-24 19:07:53 +1000

    Copy attachment/avatar data to bucket if configured as such.

--- src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/home/SharedHomeFormula.kt
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/home/SharedHomeFormula.kt
@@ -3,18 +3,26 @@ package com.atlassian.performance.tools.awsinfrastructure.jira.home
 import com.atlassian.performance.tools.aws.api.Storage
 import com.atlassian.performance.tools.awsinfrastructure.api.aws.AwsCli
 import com.atlassian.performance.tools.awsinfrastructure.api.hardware.Computer
+import com.atlassian.performance.tools.awsinfrastructure.api.jira.JiraSharedStorageResource
+import com.atlassian.performance.tools.awsinfrastructure.api.jira.JiraSharedStorageResource.ATTACHMENTS
+import com.atlassian.performance.tools.awsinfrastructure.api.jira.JiraSharedStorageResource.AVATARS
 import com.atlassian.performance.tools.infrastructure.api.jira.JiraHomeSource
 import com.atlassian.performance.tools.infrastructure.api.jira.SharedHome
 import com.atlassian.performance.tools.infrastructure.api.os.Ubuntu
 import com.atlassian.performance.tools.ssh.api.Ssh
 import java.time.Duration
+import java.util.*
 
 internal class SharedHomeFormula(
     private val pluginsTransport: Storage,
     private val jiraHomeSource: JiraHomeSource,
     private val ip: String,
     private val ssh: Ssh,
-    private val computer: Computer
+    private val computer: Computer,
+    private val storeInS3: EnumSet<JiraSharedStorageResource> = EnumSet.of(
+        ATTACHMENTS
+    ),
+    private val s3StorageBucketName: String? = null
 ) {
     private val localSubnet = "10.0.0.0/24"
     private val localSharedHome = "/home/ubuntu/jira-shared-home"
@@ -22,10 +30,12 @@ internal class SharedHomeFormula(
     private val ubuntu = Ubuntu()
 
     fun provision(): SharedHome {
+        val awsCliVersion = "2.9.12"
+
         ssh.newConnection().use {
             computer.setUp(it)
             val jiraHome = jiraHomeSource.download(it)
-            AwsCli().download(
+            AwsCli(awsCliVersion).download(
                 location = pluginsTransport.location,
                 ssh = it,
                 target = "$jiraHome/plugins/installed-plugins",
@@ -37,6 +47,31 @@ internal class SharedHomeFormula(
             it.safeExecute("sudo mv $jiraHome/logos $localSharedHome")
             ubuntu.install(it, listOf("nfs-kernel-server"))
             it.execute("sudo echo '$localSharedHome $localSubnet(rw,sync,no_subtree_check,no_root_squash)' | sudo tee -a /etc/exports")
+
+            if (s3StorageBucketName != null && storeInS3.isNotEmpty()) {
+                AwsCli(awsCliVersion).ensureAwsCli(it)
+                if (storeInS3.contains(ATTACHMENTS)) {
+                    // Copy the attachment data from NFS onto S3
+                    // Use xargs to split the work across 30 concurrent jobs.  This is much faster than a single job.
+                    it.safeExecute(
+                        cmd = "export AWS_RETRY_MODE=standard; export AWS_MAX_ATTEMPTS=10; cd $localSharedHome/data && ( find attachments -mindepth 1 -maxdepth 1 -type d -print0 | xargs -n1 -0 -P30 -I {} aws s3 cp --recursive --only-show-errors {}/ s3://$s3StorageBucketName/{}/ )",
+                        timeout = Duration.ofMinutes(10)
+                    )
+                }
+                if (storeInS3.contains(AVATARS)) {
+                    // Copy the avatar data from NFS onto S3
+                    // Split up into subdirs for faster s3 copy then use xargs to split the work across 30 concurrent jobs.
+                    it.safeExecute(
+                        cmd = "cd $localSharedHome/data/avatars && ( find . -mindepth 1 -maxdepth 1 -type f -print0 | xargs -0 -n 1000 bash -c 'dir=\"subdir_\$(date +%s%N)\"; mkdir -p \"\$dir\"; mv \"\${@:1}\" \"\$dir\"' _ )",
+                        timeout = Duration.ofMinutes(3)
+                    )
+                    it.safeExecute(
+                        cmd = "export AWS_RETRY_MODE=standard; export AWS_MAX_ATTEMPTS=10; cd $localSharedHome/data && ( find avatars -mindepth 1 -maxdepth 1 -type d -print0 | xargs -n1 -0 -P30 -I {} aws s3 cp --recursive --only-show-errors {}/ s3://$s3StorageBucketName/avatars )",
+                        timeout = Duration.ofMinutes(10)
+                    )
+                }
+            }
+
             it.execute("sudo service nfs-kernel-server restart")
         }
 
