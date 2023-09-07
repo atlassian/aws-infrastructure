aa1f9da7be0adf62229994933060f6c743107e87
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-09-07 21:49:11 +0200

    JPERF-273 Download report on failed test

--- src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/jira/HooksDataCenterFormulaIT.kt
+++ src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/api/jira/HooksDataCenterFormulaIT.kt
@@ -13,20 +13,30 @@ import com.atlassian.performance.tools.infrastructure.api.jira.JiraLaunchTimeout
 import com.atlassian.performance.tools.infrastructure.api.jira.install.ParallelInstallation
 import com.atlassian.performance.tools.infrastructure.api.jira.install.hook.PreInstallHooks
 import com.atlassian.performance.tools.infrastructure.api.jira.instance.JiraDataCenterPlan
+import com.atlassian.performance.tools.infrastructure.api.jira.instance.JiraInstance
 import com.atlassian.performance.tools.infrastructure.api.jira.instance.JiraNodePlan
 import com.atlassian.performance.tools.infrastructure.api.jira.instance.PreInstanceHooks
+import com.atlassian.performance.tools.infrastructure.api.jira.report.Reports
 import com.atlassian.performance.tools.infrastructure.api.jira.sharedhome.NfsSharedHome
 import com.atlassian.performance.tools.infrastructure.api.jira.start.JiraLaunchScript
+import com.atlassian.performance.tools.infrastructure.api.jira.start.StartedJira
+import com.atlassian.performance.tools.infrastructure.api.jira.start.hook.PostStartHook
+import com.atlassian.performance.tools.infrastructure.api.jira.start.hook.PostStartHooks
 import com.atlassian.performance.tools.infrastructure.api.jira.start.hook.RestUpgrade
 import com.atlassian.performance.tools.infrastructure.api.jvm.AdoptOpenJDK
+import com.atlassian.performance.tools.ssh.api.SshConnection
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions
 import org.junit.Test
 import java.net.URI
 import java.nio.file.Files
 import java.time.Duration
 
 class HooksDataCenterFormulaIT {
-
+    private val logger: Logger = LogManager.getLogger(this::class.java)
+    private val jiraVersion = "8.13.0"
     private val workspace = IntegrationTestRuntime.taskWorkspace.isolateTest(javaClass.simpleName).directory
     private val datasetUri = URI("https://s3-eu-west-1.amazonaws.com/")
         .resolve("jpt-custom-datasets-storage-a008820-datasetbucket-1sjxdtrv5hdhj/")
@@ -63,13 +73,21 @@ class HooksDataCenterFormulaIT {
             .setPassword("admin", "admin")
             .resetCaptcha("admin")
             .build()
+        val startingNodeLogHook = object : PostStartHook {
+            override fun call(ssh: SshConnection, jira: StartedJira, hooks: PostStartHooks, reports: Reports) {
+                logger.info("Jira node is starting at ${jira.installed.http.addressPublicly()}")
+            }
+        }
         val upgrade = RestUpgrade(JiraLaunchTimeouts.Builder().build(), "admin", "admin")
-        val installation = ParallelInstallation(jiraHome, PublicJiraSoftwareDistribution("8.13.0"), AdoptOpenJDK())
+        val installation = ParallelInstallation(jiraHome, PublicJiraSoftwareDistribution(jiraVersion), AdoptOpenJDK())
         val nodePlans = (1..2).map {
             JiraNodePlan.Builder(infra.jiraNodesServerRoom)
                 .installation(installation)
                 .start(JiraLaunchScript())
-                .hooks(PreInstallHooks.default().also { it.postStart.insert(upgrade) })
+                .hooks(PreInstallHooks.default().also {
+                    it.postStart.insert(startingNodeLogHook)
+                    it.postStart.insert(upgrade)
+                })
                 .build()
         }
         val dcPlan = JiraDataCenterPlan.Builder(nodePlans, infra.balance(ApacheEc2LoadBalancerFormula()))
@@ -81,7 +99,14 @@ class HooksDataCenterFormulaIT {
             .build()
 
         // when
-        val dataCenter = dcPlan.materialize()
+        val dataCenter: JiraInstance
+        try {
+            dataCenter = dcPlan.materialize()
+        } catch (e: Exception) {
+            dcPlan.report().downloadTo(workspace)
+            logger.error("Failed to provision DC. Report downloaded to $workspace")
+            throw e
+        }
 
         // then
         dataCenter.nodes.forEach { node ->
@@ -102,16 +127,18 @@ class HooksDataCenterFormulaIT {
             .walkTopDown()
             .map { reports.toPath().relativize(it.toPath()) }
             .toList()
-        assertThat(fileTree.map { it.toString() }).contains(
-            "jira-node-1/atlassian-jira-software-8.13.0-standalone/logs/catalina.out",
-            "jira-node-1/~/jpt-jstat.log",
-            "jira-node-2/atlassian-jira-software-8.13.0-standalone/logs/catalina.out"
-        )
-        assertThat(fileTree.filter { it.fileName.toString() == "atlassian-jira.log" })
-            .`as`("Jira log from $fileTree")
-            .isNotEmpty
-        assertThat(fileTree.filter { it.fileName.toString().startsWith("atlassian-jira-gc") })
-            .`as`("GC logs from $fileTree")
-            .isNotEmpty
+        SoftAssertions().apply {
+            assertThat(fileTree.map { it.toString() }).contains(
+                "jira-node-1/atlassian-jira-software-$jiraVersion-standalone/logs/catalina.out",
+                "jira-node-1/~/jpt-jstat.log",
+                "jira-node-2/atlassian-jira-software-$jiraVersion-standalone/logs/catalina.out"
+            )
+            assertThat(fileTree.filter { it.fileName.toString() == "atlassian-jira.log" })
+                .`as`("Jira log from $fileTree")
+                .isNotEmpty
+            assertThat(fileTree.filter { it.fileName.toString().startsWith("atlassian-jira-gc") })
+                .`as`("GC logs from $fileTree")
+                .isNotEmpty
+        }.assertAll()
     }
 }
