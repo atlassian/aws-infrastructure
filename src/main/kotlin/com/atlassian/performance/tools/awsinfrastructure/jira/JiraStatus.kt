3ef1e655475bb58274a345de380819ee0b430ecc
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2024-06-11 18:03:24 +0200

    Cover the CI IT failure with unit test
    TDD: red

---
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/JiraStatus.kt
@@ -0,0 +1,24 @@
+package com.atlassian.performance.tools.awsinfrastructure.jira
+
+import java.io.StringReader
+import javax.json.Json
+
+
+enum class JiraStatus {
+    STARTING,
+    RUNNING;
+
+    object Parser {
+
+        fun parseResponse(response: String): JiraStatus? {
+            return Json.createParser(StringReader(response)).use { jsonParser ->
+                if (jsonParser.hasNext()) {
+                    JiraStatus.valueOf(jsonParser.`object`.getString("state"))
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+}
