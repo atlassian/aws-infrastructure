f4467e5c9adb46e2fa485e5c00f224564bfcd5b9
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2024-06-11 18:08:42 +0200

    Refactor JiraStatus.Parser
    Replace "iterator" style with "sequence" style
    
    TDD: refactor

--- src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/JiraStatus.kt
+++ src/main/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/JiraStatus.kt
@@ -2,6 +2,7 @@ package com.atlassian.performance.tools.awsinfrastructure.jira
 
 import java.io.StringReader
 import javax.json.Json
+import kotlin.streams.asSequence
 
 
 enum class JiraStatus {
@@ -12,14 +13,14 @@ enum class JiraStatus {
 
         fun parseResponse(response: String): JiraStatus? {
             return Json.createParser(StringReader(response)).use { jsonParser ->
-                if (jsonParser.hasNext()) {
-                    jsonParser.next()
-                    JiraStatus.valueOf(jsonParser.`object`.getString("state"))
-                } else {
-                    null
-                }
+                jsonParser
+                    .valueStream
+                    .asSequence()
+                    .firstOrNull()
+                    ?.asJsonObject()
+                    ?.getString("state")
+                    ?.let { JiraStatus.valueOf(it) }
             }
         }
     }
-
 }
