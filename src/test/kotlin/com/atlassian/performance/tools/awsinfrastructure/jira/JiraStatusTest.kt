96b7657a9cc300e9f2c979df3dd3128571c52ff6
Maciej Kwidzinski <mkwidzinski@atlassian.com>
2024-06-14 19:12:28 +0200

    Repro parsing random JSONs
    TDD: red

--- src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/JiraStatusTest.kt
+++ src/test/kotlin/com/atlassian/performance/tools/awsinfrastructure/jira/JiraStatusTest.kt
@@ -1,9 +1,19 @@
 package com.atlassian.performance.tools.awsinfrastructure.jira
 
+import com.atlassian.performance.tools.awsinfrastructure.LogConfigurationFactory
+import com.atlassian.performance.tools.workspace.api.RootWorkspace
+import org.apache.logging.log4j.core.config.ConfigurationFactory
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class JiraStatusTest {
+
+    @Before
+    fun setUpLogs() {
+        ConfigurationFactory.setConfigurationFactory(LogConfigurationFactory(RootWorkspace().currentTask))
+    }
+
     @Test
     fun shouldParseRunning() {
         val actual = JiraStatus.Parser.parseResponse("""{"state":"RUNNING"}""")
@@ -48,6 +58,20 @@ class JiraStatusTest {
     @Test
     fun shouldParseGarbled() {
         val actual = JiraStatus.Parser.parseResponse("""<html>SURPRISE</html>""")
+
+        assertThat(actual).isNull()
+    }
+
+    @Test
+    fun shouldParseRandomJson() {
+        val actual = JiraStatus.Parser.parseResponse("""[{"banana": "sure"}]""")
+
+        assertThat(actual).isNull()
+    }
+
+    @Test
+    fun shouldParseRandomJsonObject() {
+        val actual = JiraStatus.Parser.parseResponse("""{"hello": "it's me"}""")
 
         assertThat(actual).isNull()
     }
